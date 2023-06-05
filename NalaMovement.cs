9e85d772b0acb9545f6f8363c5e6ee56866b3ae3
Rj27yesman <106731751+Rj27yesman@users.noreply.github.com>
2023-06-05 13:22:56 +0800

    Add files via upload

---
+++ NalaMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NalaMovement : MonoBehaviour {
+
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+    // Start is called before the first frame update
+    float horizontalMove = 0f;
+    bool jump = false;
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+       animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+       if(Input.GetButtonDown("Jump"))
+       {
+       
+         jump = true;
+         animator.SetBool("IsJumping", true);
+       }
+       
+
+    }
+    public void OnLanding ()
+    {
+       animator.SetBool("IsJumping", false);
+    }
+       
+
+    void FixedUpdate ()
+    {
+      controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+      jump = false;
+
+
+     
+
+
+
+
+  
+
+    }
+
+
+
+}
+
